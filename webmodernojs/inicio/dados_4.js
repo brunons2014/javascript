164a0b1644a8d6e843081b49364579a908493483
brunons2014 <62818096+brunons2014@users.noreply.github.com>
2022-03-29 23:14:07 -0300

    Logica básica com js

---
+++ webmodernojs/inicio/dados_4.js
@@ -0,0 +1,10 @@
+const a = 3;
+
+// ...
+
+// para um programa é mais saudavel você tem mais constantes do que variaveis
+
+// ...
+
+console.log(a)
+
---
+++ webmodernojs/inicio/desafio_area.js
@@ -0,0 +1,6 @@
+// PI * raio * raio
+
+let raio = 10;
+let PI = 3.14;
+
+console.log("A area do raio é " + PI * raio * raio + "²")
