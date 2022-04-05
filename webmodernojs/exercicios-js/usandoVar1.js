28d3008fd0852bc4237c32322e11157c27ad63ef
brunons2014 <62818096+brunons2014@users.noreply.github.com>
2022-04-04 21:52:39 -0300

    variaveis

---
+++ webmodernojs/exercicios-js/usandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var sera = 'Sera???'
+            }
+        }
+    }
+}
+console.log(sera)
+
+//a variavel var desde que não seja uma função ficará disponivel a todos os locais até que não seja dentro do seu bloco
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
---
+++ webmodernojs/exercicios-js/usandoVar2.js
@@ -0,0 +1,8 @@
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+//aqui sempre será 2 pois o var sempre sobreescreve o anterior
